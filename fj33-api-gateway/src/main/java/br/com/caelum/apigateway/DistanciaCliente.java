8f9ee0f3e3bb825e616b295ab223e6cd8d3c3080
Alefh Sousa <alefh.sousa@gmail.com>
2020-08-05 02:15:26 -0300

    add service discovery to gateway

--- fj33-api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaCliente.java
+++ fj33-api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaCliente.java
@@ -1,27 +1,44 @@
 package br.com.caelum.apigateway;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class DistanciaCliente {
 
     private String distanciaServiceUrl;
     private RestTemplate restTemplate;
+    private CacheManager cacheManager;
 
     DistanciaCliente(RestTemplate restTemplate,
-                        @Value("${zuul.routes.distancia.url}") String distanciaServiceUrl) {
+                        @Value("${configuracao.distancia.service.url}") String distanciaServiceUrl) {
         this.distanciaServiceUrl = distanciaServiceUrl;
         this.restTemplate = restTemplate;
     }
 
-
+    @HystrixCommand(fallbackMethod = "buscaDistanciaPorRestauranteIdECepFallback")
     public Map<String, Object> buscaDistanciaPorRestauranteIdECep(String cep, Long restauranteId) {
         final String url = distanciaServiceUrl + "/restaurantes/" + cep + "/restaurante/" + restauranteId;
         return restTemplate.getForObject(url, Map.class);
+    }
+
+
+    public Map<String, Object> buscaDistanciaPorRestauranteIdECepFallback(String cep, Long restauranteId) {
+        log.info("chamando metodo de fallback");
+        HashMap<String, Object> retornoFallback = new HashMap<>();
+
+        retornoFallback.put("restauranteId", restauranteId);
+        retornoFallback.put("distancia", BigDecimal.ZERO);
+        return retornoFallback;
     }
 
 }
--- fj33-api-gateway/src/main/java/br/com/caelum/apigateway/RestauranteCliente.java
+++ fj33-api-gateway/src/main/java/br/com/caelum/apigateway/RestauranteCliente.java
@@ -1,16 +1,33 @@
 package br.com.caelum.apigateway;
 
+import feign.Response;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@FeignClient("monolito")
+@FeignClient(name = "monolito", fallback = RestauranteClientFallback.class)
 public interface RestauranteCliente {
 
 
     @GetMapping("/restaurantes/{id}")
     Map<String, Object> buscaRestaurantePorId(@PathVariable("id") Long restauranteId);
 
+}
+
+
+
+@Component
+class RestauranteClientFallback implements RestauranteCliente {
+
+    @Override
+    public Map<String, Object> buscaRestaurantePorId(Long restauranteId) {
+        HashMap<String, Object> retornoFallback = new HashMap<>();
+        retornoFallback.put("id", restauranteId);
+        return retornoFallback;
+    }
 }
